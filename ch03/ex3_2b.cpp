08cfcc096e7ca358d14a5771e28da0552772b7fe
gong7788 <44057714+gong7788@users.noreply.github.com>
2019-12-29 20:54:39 +0000

    Create ex3_2b.cpp

---
+++ ch03/ex3_2b.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cin; using std::cout; using std::endl;
+
+int main() {
+    for(string word; cin >> word; cout << word << endl);
+    return 0;
+}
