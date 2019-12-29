571c82fe22a8cf96a2571cf22c078fc27efda87a
gong7788 <44057714+gong7788@users.noreply.github.com>
2019-12-29 21:26:19 +0000

    Create ex3_5b.cpp

---
+++ ch03/ex3_5b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cin; using std::cout; using std::endl;
+
+int main() {
+    string result, input;
+    while (cin >> input) {
+        if (input == ".")
+            break;
+        else
+            result += (input.empty() ? "" : " ") + input;
+    }
+    cout << result << endl;
+    return 0;
+}
